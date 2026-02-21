9a5a91c6914e7ff96128106d0c2c7b1e9315c656
perussigabi <perussigabi2@gmail.com>
2026-02-21 16:39:57 -0300

    Add files via upload

---
+++ CursoProgramacao/CursoProgramacao/Class2.cs
@@ -0,0 +1,16 @@
+﻿namespace CursoProgramacao
+{
+    public class Class2
+    {
+        public void ValorPar()
+        {
+            int numero = 5;
+            int resultado = 0;
+
+            resultado = numero % 2;
+
+            Console.WriteLine(resultado);
+        }
+
+    }
+}
